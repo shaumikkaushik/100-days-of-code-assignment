781eccffaf83cb4d8a29ee0469b2b4e581623e30
Shaumik Kaushik <shaumikkaushik@gmail.com>
2026-08-26 18:00:00 +0530

    Day 17 Solutions

---
+++ 100 days of code/Day17/Q33.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, original, temp, rem, digits = 0;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+    temp = n;
+
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+    while (temp != 0) {
+        rem = temp % 10;
+        sum += round(pow(rem, digits));
+        temp /= 10;
+    }
+
+    if (sum == original) {
+        printf("%d is an Armstrong number\n", original);
+    } else {
+        printf("%d is not an Armstrong number\n", original);
+    }
+
+    return 0;
