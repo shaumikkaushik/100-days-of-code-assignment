3c990915ee3232b38e47b733e197e55baceb74d3
Shaumik Kaushik <shaumikkaushik@gmail.com>
2026-09-09 18:00:00 +0530

    Day 31 Solutions

---
+++ 100 days of code/Day31/Q62.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Two-pointer swap
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
